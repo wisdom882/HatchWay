4e96f39a73a5ef5746f4937f98cea3e11f81a4d3
king <miabiyewisdom@gmail.com>
2022-05-03 15:40:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[persons, setPersons] = React.useState([]);
+  const[searchField, setSearchField] = React.useState('');
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const personsData = await response.json();
+      setPersons(personsData);
+    }
+
+    fetchData();
+   
+  })
+
+
+
+  return(
+    
+      <div className="App">
+        <h1>Face Cards</h1>
+        <SearchBox 
+        placeholder="search persons"
+        handleChange = {e=>{setSearchField(e.target.value)}}
+        />
+        <CardList persons={ persons.filter( person => person.name.toLowerCase().includes(searchField.toLowerCase()))} />
+      </div>
+    
+  )
 }
+
 
 export default App;
---
+++ src/components/list/list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from '../card/card.component';
+
+import './card-list.styles.css';
+
+export const CardList = props  =>  (
+    <div className ="card-list">
+         {
+          props.persons.map(person => (
+                <Card key={person.id} person={person} />
+          ))
+        }
+    </div>
+)
